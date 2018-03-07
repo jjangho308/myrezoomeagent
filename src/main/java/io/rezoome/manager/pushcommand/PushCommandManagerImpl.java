e0c387fc552f79dc36ebb2307262420ef6f30a28
안택수(An Tacksu) <ahnzaz@gmail.com>
2018-03-07 20:24:49 +0900

    test

--- src/main/java/io/rezoome/manager/pushcommand/PushCommandManagerImpl.java
+++ src/main/java/io/rezoome/manager/pushcommand/PushCommandManagerImpl.java
@@ -54,10 +54,13 @@ public class PushCommandManagerImpl extends AbstractManager implements PushComma
 		// TODO Auto-generated method stub
 
 	}
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public PushCommandResult invokeCommand(PushCommandEntity command) {
-    PushCommandAction<PushCommandEntity> action = (PushCommandAction<PushCommandEntity>) this.actionMap.get(command.getClass());
+		PushCommandAction<PushCommandEntity> action =
+				(PushCommandAction<PushCommandEntity>) this.actionMap
+				.get(command.getClass());
 		action.process(command);
 		return null;
 	}
@@ -65,7 +68,7 @@ public class PushCommandManagerImpl extends AbstractManager implements PushComma
 	@Override
 	public <T extends PushCommandEntity> PushCommandAction<T> getAction(T command) {
 		// TODO Auto-generated method stub
-	  
+
 		return null;
 	}
 
