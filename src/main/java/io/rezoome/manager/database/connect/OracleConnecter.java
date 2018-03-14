5e18bdfeb00e47a238f9b1ecb52789df79abe36f
pkingkong <im.pkingkong@gmail.com>
2018-03-14 15:31:37 +0900

    modify oracle setting

--- src/main/java/io/rezoome/manager/database/connect/OracleConnecter.java
+++ src/main/java/io/rezoome/manager/database/connect/OracleConnecter.java
@@ -2,11 +2,12 @@ package io.rezoome.manager.database.connect;
 
 public class OracleConnecter extends DBConnectionManagerImpl {
 
-	public OracleConnecter() {
-		String JDBCDriver = "oracle.jdbc.driver.OracleDriver";
-		String JDBCDriverType = "jdbc:oracle://";
-		String url = JDBCDriverType + ":@" + dbHost + ":" + dbPort + "/" + dbName;
-		connMgr = DBConnectionPoolManager.getInstance();
-		connMgr.init(poolName, JDBCDriver, url, dbUserID, dbPasswd, maxConn, initConn, maxWait);
-	}
+  public OracleConnecter() {
+    String JDBCDriver = "oracle.jdbc.driver.OracleDriver";
+    // String JDBCDriverType = "jdbc:oracle://";
+    String JDBCDriverType = "jdbc:oracle:thin";
+    String url = JDBCDriverType + ":@" + dbHost + ":" + dbPort + "/" + dbName;
+    connMgr = DBConnectionPoolManager.getInstance();
+    connMgr.init(poolName, JDBCDriver, url, dbUserID, dbPasswd, maxConn, initConn, maxWait);
+  }
 }
