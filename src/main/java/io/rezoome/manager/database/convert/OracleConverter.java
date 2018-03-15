9e9693edb5b682643d8a2aedfb2f0bccc6bab480
newoosy <exle1004@gmail.com>
2018-03-15 10:55:27 +0900

    YSY

--- src/main/java/io/rezoome/manager/database/convert/OracleConverter.java
+++ src/main/java/io/rezoome/manager/database/convert/OracleConverter.java
@@ -2,6 +2,7 @@ package io.rezoome.manager.database.convert;
 
 import io.rezoome.lib.json.JSON;
 import io.rezoome.manager.database.convert.entity.MySQLEntity;
+import io.rezoome.manager.database.convert.entity.OracleEntity;
 import io.rezoome.manager.database.entity.DBEntity;
 import io.rezoome.manager.job.entity.AbstractJobEntity;
 
@@ -12,7 +13,7 @@ public class OracleConverter extends DBConvertManagerImpl implements DBConverter
   public DBEntity convert(AbstractJobEntity job) {
     // TODO Auto-generated method stub
 
-    DBEntity entity =  JSON.fromJson(job.toJSON(), MySQLEntity.class);
+    DBEntity entity =  JSON.fromJson(job.toJSON(), DBEntity.class);
     
     System.out.println("Oracle Converter : " + entity);
     return entity;
