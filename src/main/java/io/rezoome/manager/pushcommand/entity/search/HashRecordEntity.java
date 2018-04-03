fc788161f109b335d6c79148802f56e3950dbf43
pkingkong <im.pkingkong@gmail.com>
2018-04-03 15:56:29 +0900

    push

--- src/main/java/io/rezoome/manager/pushcommand/entity/search/HashRecordEntity.java
+++ src/main/java/io/rezoome/manager/pushcommand/entity/search/HashRecordEntity.java
@@ -1,7 +1,5 @@
 package io.rezoome.manager.pushcommand.entity.search;
 
-import java.util.List;
-
 import com.google.gson.annotations.SerializedName;
 
 public final class HashRecordEntity {
@@ -10,13 +8,13 @@ public final class HashRecordEntity {
   private final String subID = null;
 
   @SerializedName("hashed")
-  private final List<String> hashed = null;
+  private final String hashed = null;
 
   public String getSubID() {
     return subID;
   }
 
-  public List<String> getHashed() {
+  public String getHashed() {
     return hashed;
   }
 
